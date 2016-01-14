ab4ab21ec87f0f48e633c9118c454adbbb8230e6
slavik <barthez.slavik@gmail.com>
2016-01-14 14:34:50 +0200

    add priority

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114060131) do
+ActiveRecord::Schema.define(version: 20160114114046) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160114060131) do
     t.date     "deadline"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "priority"
   end
 
 end
