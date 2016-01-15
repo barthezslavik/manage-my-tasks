8e06452b5db5e316b44f68314af90ecffabd9088
slavik <barthez.slavik@gmail.com>
2016-01-15 09:24:03 +0200

    angular projects

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,5 +1,7 @@
-var application = angular.module('application',[]);
-
-application.controller('ProjectController', ['$scope', function($scope) {
-  $scope.greeting = 'Hola!';
+var app = angular.module('application',[])
+  .controller('ProjectController', ['$scope', '$http', function($scope, $http) {
+  $http.get('http://localhost:3000/projects.json').
+    success(function(data) {
+      $scope.projects = data;
+  });
 }]);
