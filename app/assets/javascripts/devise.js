8ff9ecec07d1a2db7359038d05cf7f413e26007a
slavik <barthez.slavik@gmail.com>
2016-01-20 13:32:23 +0200

    sign_up

--- app/assets/javascripts/devise.js
+++ app/assets/javascripts/devise.js
@@ -13,10 +13,30 @@ app.controller('SignInController',function($scope,Auth){
     };
 
     Auth.login(credentials, config).then(function(user) {
-      console.log(user); // => {id: 1, ect: '...'}
+      console.log(user);
     }, function(error) {
       $scope.error = error.data.error;
-      // Authentication failed...
+    });
+  }
+})
+
+app.controller('SignUpController',function($scope,Auth){
+  $scope.signUp=function(){
+    var credentials = {
+      email: $scope.email,
+      password: $scope.password,
+      password_confirmation: $scope.password_confirmation,
+    };
+    var config = {
+      headers: {
+        'X-HTTP-Method-Override': 'POST'
+      }
+    };
+
+    Auth.register(credentials, config).then(function(user) {
+      console.log(user);
+    }, function(error) {
+      $scope.error = error.data.error;
     });
   }
 })
